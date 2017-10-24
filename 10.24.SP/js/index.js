920b92e9735749ac2652ae55bffadd1f5296898f
TTNWebEditor <web@temple-news.com>
2017-10-23 20:46:06 -0400

    Add files via upload

---
+++ 10.24.SP/js/index.js
@@ -0,0 +1,7 @@
+$("#button").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+
+$("#wbutton").click(function() {
+  $('.wtransform').toggleClass('wtransform-active');
+});
